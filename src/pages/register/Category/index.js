d2f7d9ae938ddee22562bf33a1ea082f03ac6a25
Thau26 <thauanyaraujoalves@hotmail.com>
2020-08-05 21:11:20 -0300

    correcao configuracao de url do server

--- src/pages/register/Category/index.js
+++ src/pages/register/Category/index.js
@@ -35,8 +35,9 @@ function RegisterCategory() {
   // ============
 
   useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      const URL = 'https://tataflix.herokuapp.com/categorias';
+      const URL =  window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://tataflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
