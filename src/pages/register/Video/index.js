e984458b3fcfabcb6a0e9783d66c3f386b7f606c
Thau26 <thauanyaraujoalves@hotmail.com>
2020-08-06 00:04:46 -0300

    criacao pagina de cadastro de video

--- src/pages/register/Video/index.js
+++ src/pages/register/Video/index.js
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
+import useForm from '../../../hoocks/useForm';
 
-function RegisterVideo(){
-    return (
-        <PageDefault>
-            <h1>Cadastro de Vídeo</h1>
-            <Link to='/register/category'>
-                    Cadastrar categoria
-            </Link>
-        </PageDefault>
-            
+function RegisterVideo() {
+  const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
+  const categoryTitles = categorias.map(({ titulo }) => titulo);
+  const { handleChange, values } = useForm({
+    titulo: 'Video padrão',
+    url: 'https://www.youtube.com/watch?v=jOAU81jdi-c',
+    categoria: 'Front End',
+  });
 
-    );
+  useEffect(() => {
+    categoriasRepository
+      .getAll()
+      .then((categoriasFromServer) => {
+        setCategorias(categoriasFromServer);
+      });
+  }, []);
+
+  return (
+    <PageDefault>
+      <h1>Cadastro de Video</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        
+        const categoriaEscolhida = categorias.find((categoria) => {
+          return categoria.titulo === values.categoria;
+        });
+
+        videosRepository.create({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: categoriaEscolhida.id,
+        })
+          .then(() => {
+            history.push('/');
+          });
+      }}
+      >
+        <FormField
+          label="Título do Vídeo"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="URL"
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Categoria"
+          name="categoria"
+          value={values.categoria}
+          onChange={handleChange}
+          suggestions={categoryTitles}
+        />
+
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
+
+      <br />
+      <br />
+
+      <Link to="/register/category">
+        Cadastrar Categoria
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default RegisterVideo;
