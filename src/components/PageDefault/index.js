b00456f589e4089d90524f2afbddc80fe62ad164
Thau26 <thauanyaraujoalves@hotmail.com>
2020-08-06 00:03:05 -0300

    correcao no padding do banner principal

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from '../Menu';
 import Footer from '../Footer';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Main = styled.main`
     background-color: var(--black);
@@ -11,15 +11,18 @@ const Main = styled.main`
     padding-top: 50px;
     padding-left: 5%;
     padding-right: 5%;
+    ${({paddingAll}) => css`
+       padding: ${paddingAll}; 
+    `}
     
 `;
 
-function PageDefault({children}){
+function PageDefault({children, paddingAll}){
 
     return (
         <>
             <Menu />
-                <Main>
+                <Main paddingAll={paddingAll} >
                     {children}
 
                 </Main>
